4c25f5cefa08eb93cead04faf0b1fd9c816357b7
Furkan Yıldırım <yildirim.frkn@outlook.com>
2022-04-22 00:04:05 +0300

    Template

---
+++ Template/WindowActivationAndDeactivation/User1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindowActivationAndDeactivation
+{
+    class User1 : UserTemplate
+    {
+        public override int UserAge()
+        {
+            return 21;
+        }
+
+        public override string UserJob()
+        {
+            return "Student";
+        }
+
+        public override string WhoUse()
+        {
+            return "Furkan";
+        }
+    }
+}
---
+++ Template/WindowActivationAndDeactivation/UserTemplate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindowActivationAndDeactivation
+{
+    abstract class UserTemplate
+    {
+
+       public string User()
+        {
+            return "User : " + WhoUse();
+        }
+        public string Age()
+        {
+            return "Age : " + UserAge();
+        }
+        public string Job()
+        {
+            return "Job : " + UserJob();
+        }
+        public string UsingTemplate()
+        {
+            return User() + "\n" + Age() + "\n" + Job();
+        }
+
+        public abstract string UserJob();
+        public abstract string WhoUse();
+        public abstract int UserAge();
+
+    }
+}
